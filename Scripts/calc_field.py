Fix B1 computation in calc_field

Scripts/calc_field.py
@@ -97,8 +97,8 @@ def calc_field(arrays_list, axis_dict, nb_elem, coil_definition = 100):
                     Ay_sum[a, b, c] = Ay_sum[a, b, c] + A_tmp[i]
                     Az_sum[a, b, c] = Az_sum[a, b, c] + A_tmp[i]
 
-    B1 = np.zeros(x_len)
-    A = np.zeros(x_len)
+    B1 = np.zeros((x_len, y_len, z_len))
+    A = np.zeros((x_len, y_len, z_len))
 
     B1 = np.sqrt(np.power(B1x_sum, 2) + np.power(B1y_sum, 2) + np.power(B1z_sum, 2))
     A = np.sqrt(np.power(Ax_sum, 2) + np.power(Ay_sum, 2) + np.power(Az_sum, 2))

Scripts/planar_array.py
@@ -77,9 +77,9 @@ for i in range(nb_elem):
     bB1f = np.sqrt(np.power(bB1f, 2) + np.power(B1_tmp, 2))
 
 B1f = np.zeros((x_len, z_len))
-B1f = bB1f[:, 1, :]
+B1f[:, :] = bB1f[:, 1, :]
 
-print(B1f)
+print(B1_tmp)
 
 """MatPlotLib calls to display the coils in 3-D"""
 fig = plt.figure()