cb9b2202acdee0b29c2af785db8bf6e3ace0c546
PL <iashiua>
2015-09-30 17:11:25 -0400

    Add the function that plot the coils in 3D

---
+++ Scripts/plot_planar_array.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from scipy import math
+from mpl_toolkits.mplot3d import Axes3D
+
+def plot_planar_array(nb_elem, arrays_list, N):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    n = 0
+    for n in range(nb_elem):
+        for i in range(N):
+            xs = arrays_list[n][i][0]
+            ys = arrays_list[n][i][1]
+            zs = arrays_list[n][i][2]
+            ax.scatter(xs, ys, zs)
+
+    ax.set_xlabel('X axis [m]')
+    ax.set_ylabel('Y axis [m]')
+    ax.set_zlabel('Z axis [m]')
+    ax.set_title('Elements distribution around a cylinder')
