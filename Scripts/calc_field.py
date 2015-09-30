1249f0905c27cb6959a73177fb65d65f582177a6
Charles <charleslariviereloiselle@gmail.com>
2015-09-30 15:49:32 -0400

    Modify matrix treatment in calc_field to obtain better results

--- Scripts/calc_field.py
+++ Scripts/calc_field.py
@@ -5,7 +5,7 @@ PI = np.pi
 """Define physics constants of the coils"""
 u0 = 4*PI*(10**-7)  # relative permittivity for air
 coil_definition = 100  # points used to define coil
-I = 1  # constant current distribution in each coil
+I = 1.0  # constant current distribution in each coil
 cond = 0.7  # define conductivity
 freq = 123.2*10**6  # define frequency
 
@@ -13,7 +13,6 @@ freq = 123.2*10**6  # define frequency
 
 
 def calc_field(arrays_list, axis_dict, nb_elem, coil_definition = 100):
-
     """Define each axis length"""
 
     x_len = (axis_dict['Xmax'] - axis_dict['Xmin']) / axis_dict['Xprec']
@@ -27,56 +26,50 @@ def calc_field(arrays_list, axis_dict, nb_elem, coil_definition = 100):
     """Generate mesh that will contain the magnetic field matrix. Mesh contains all the
     values of the axis for X, Y, Z."""
     for i in range(x_len):
-        for j in range(y_len):
-            for k in range(z_len):
-                x_matrix[i][j][k] = axis_dict['Xmin'] + i*axis_dict['Xprec']
+        x_matrix[i, :, :] = axis_dict['Xmin'] + i*axis_dict['Xprec']
     for i in range(y_len):
-        for j in range(x_len):
-            for k in range(z_len):
-                y_matrix[j][i][k] = axis_dict['Ymin'] + i*axis_dict['Yprec']
+        y_matrix[:, i, :] = axis_dict['Ymin'] + i*axis_dict['Yprec']
     for i in range(z_len):
-        for j in range(x_len):
-            for k in range(y_len):
-                z_matrix[j][k][i] = axis_dict['Zmin'] + i*axis_dict['Zprec']
+        z_matrix[:, :, i] = axis_dict['Zmin'] + i*axis_dict['Zprec']
 
     """Declaring r and dl vectors for future Biot-Savart computation"""
-    r = np.zeros((coil_definition, 3))
+    r = np.zeros((3, coil_definition))
 
-    dl = np.zeros((coil_definition, 3))
-    dl_cross_r = np.zeros((coil_definition, 3))
-    norm_r = np.zeros((coil_definition, 1))
+    dl = np.zeros((3, coil_definition))
+    dl_cross_r = np.zeros((3, coil_definition))
+    norm_r = np.zeros(coil_definition)
     for a in range(x_len):
         for b in range(y_len):
             for c in range(z_len):
                 for i in range(coil_definition-1):
                     """Computes vector R between point on the coil and the observation point
                     We use the middle point between each point of the coil's array"""
-                    r[i][0] = (x_matrix[a, b, c] - (0.5*(arrays_list[nb_elem][i][0]+arrays_list[nb_elem][i+1][0])))
-                    r[i][1] = (y_matrix[a, b, c] - (0.5*(arrays_list[nb_elem][i][1]+arrays_list[nb_elem][i+1][1])))
-                    r[i][2] = (z_matrix[a, b, c] - (0.5*(arrays_list[nb_elem][i][2]+arrays_list[nb_elem][i+1][2])))
+                    r[0, i] = (x_matrix[a, b, c] - (0.5*(arrays_list[nb_elem][i][0]+arrays_list[nb_elem][i+1][0])))
+                    r[1, i] = (y_matrix[a, b, c] - (0.5*(arrays_list[nb_elem][i][1]+arrays_list[nb_elem][i+1][1])))
+                    r[2, i] = (z_matrix[a, b, c] - (0.5*(arrays_list[nb_elem][i][2]+arrays_list[nb_elem][i+1][2])))
 
                     """Computes vector dl along the coil in direction of flow of current
                     We use the middle point between each point of the coil's array"""
-                    dl[i][0] = arrays_list[nb_elem][i+1][0]+arrays_list[nb_elem][i][0]
-                    dl[i][1] = arrays_list[nb_elem][i+1][1]+arrays_list[nb_elem][i][1]
-                    dl[i][2] = arrays_list[nb_elem][i+1][2]+arrays_list[nb_elem][i][2]
+                    dl[0, i] = arrays_list[nb_elem][i+1][0]+arrays_list[nb_elem][i][0]
+                    dl[1, i] = arrays_list[nb_elem][i+1][1]+arrays_list[nb_elem][i][1]
+                    dl[2, i] = arrays_list[nb_elem][i+1][2]+arrays_list[nb_elem][i][2]
 
                 """We compute vectors between final and initial points outside of the loop"""
-                r[coil_definition-1][0] = (x_matrix[a, b, c] - (0.5*(arrays_list[nb_elem][coil_definition-1][0]+arrays_list[nb_elem][0][0])))
-                r[coil_definition-1][1] = (y_matrix[a, b, c] - (0.5*(arrays_list[nb_elem][coil_definition-1][1]+arrays_list[nb_elem][0][1])))
-                r[coil_definition-1][2] = (z_matrix[a, b, c] - (0.5*(arrays_list[nb_elem][coil_definition-1][2]+arrays_list[nb_elem][0][2])))
+                r[0, coil_definition-1] = (x_matrix[a, b, c] - (0.5*(arrays_list[nb_elem][coil_definition-1][0]+arrays_list[nb_elem][0][0])))
+                r[1, coil_definition-1] = (y_matrix[a, b, c] - (0.5*(arrays_list[nb_elem][coil_definition-1][1]+arrays_list[nb_elem][0][1])))
+                r[2, coil_definition-1] = (z_matrix[a, b, c] - (0.5*(arrays_list[nb_elem][coil_definition-1][2]+arrays_list[nb_elem][0][2])))
 
-                dl[coil_definition-1][0] = arrays_list[nb_elem][coil_definition-1][0]+arrays_list[nb_elem][0][0]
-                dl[coil_definition-1][1] = arrays_list[nb_elem][coil_definition-1][1]+arrays_list[nb_elem][0][1]
-                dl[coil_definition-1][2] = arrays_list[nb_elem][coil_definition-1][2]+arrays_list[nb_elem][0][2]
+                dl[0, coil_definition-1] = arrays_list[nb_elem][coil_definition-1][0]+arrays_list[nb_elem][0][0]
+                dl[1, coil_definition-1] = arrays_list[nb_elem][coil_definition-1][1]+arrays_list[nb_elem][0][1]
+                dl[2, coil_definition-1] = arrays_list[nb_elem][coil_definition-1][2]+arrays_list[nb_elem][0][2]
     """Computing dl cross r and norm of r"""
     for i in range(coil_definition):
-        dl_cross_r[i] = np.cross(r[i], dl[i])
-        norm_r[i] = np.linalg.norm(r[i])
+        dl_cross_r[:, i] = np.cross(dl[:, i], r[:, i])
+        norm_r[i] = np.linalg.norm(r[:, i])
     
     """Biot-Savart's law analytical resolution"""
-    A_tmp = np.divide(I*u0, np.power(norm_r, 3))
-    B1_tmp = np.multiply(A_tmp, dl_cross_r)
+    A_tmp = np.divide(I*u0, 4*PI*norm_r)
+    B1_tmp = np.multiply(np.divide(I*u0, 4*PI*np.power(norm_r, 3)), dl_cross_r)
 
     B1x_sum = np.zeros((x_len, y_len, z_len))
     B1y_sum = np.zeros((x_len, y_len, z_len))
@@ -90,9 +83,9 @@ def calc_field(arrays_list, axis_dict, nb_elem, coil_definition = 100):
         for b in range(y_len):
             for c in range(z_len):
                 for i in range(coil_definition):
-                    B1x_sum[a, b, c] = B1x_sum[a, b, c] + B1_tmp[i, 0]
-                    B1y_sum[a, b, c] = B1y_sum[a, b, c] + B1_tmp[i, 1]
-                    B1z_sum[a, b, c] = B1z_sum[a, b, c] + B1_tmp[i, 2]
+                    B1x_sum[a, b, c] = B1x_sum[a, b, c] + B1_tmp[0, i]
+                    B1y_sum[a, b, c] = B1y_sum[a, b, c] + B1_tmp[1, i]
+                    B1z_sum[a, b, c] = B1z_sum[a, b, c] + B1_tmp[2, i]
                     Ax_sum[a, b, c] = Ax_sum[a, b, c] + A_tmp[i]
                     Ay_sum[a, b, c] = Ay_sum[a, b, c] + A_tmp[i]
                     Az_sum[a, b, c] = Az_sum[a, b, c] + A_tmp[i]
