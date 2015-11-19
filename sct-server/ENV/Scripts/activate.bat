8fa4d51c790e727ee79ac6b51bc6a32719a98acd
Charles <charleslariviereloiselle@gmail.com>
2015-11-19 14:58:30 -0500

    Test server functionnality

--- sct-server/ENV/Scripts/activate.bat
+++ sct-server/ENV/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\Charles\Documents\poly\A2015\spinalcordtoolbox_web-master\ENV"
+set "VIRTUAL_ENV=C:\Users\Charles\Documents\GitHub\coil_simulation_web\sct-server\ENV"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
